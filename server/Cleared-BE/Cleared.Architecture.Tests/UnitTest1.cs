04197021f604120b3ad1dfde8ef3b0aea37f7fc9
ayandabiyela <ayanda.biyela+lnlp@lexisnexis.co.za>
2026-08-27 14:44:59 +0200

    Added test class library, that will allow me to write all my unit tests

---
+++ server/Cleared-BE/Cleared.Architecture.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Cleared.Architecture.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
